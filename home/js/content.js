135c3173169ca7581d1c57b866ff995d00933ee5
Eddy Gonzalez <gon19005@byui.edu>
2022-03-19 01:30:57 -0600

    updated information and images

---
+++ home/js/content.js
@@ -0,0 +1,93 @@
+
+const title_page = document.getElementById("title_page");
+const main_title = document.getElementById("main_title");
+const main_message = document.getElementById("main_message");
+const quote = document.getElementById("quote");
+const contact = document.getElementById("contact");
+const about_title = document.getElementById("about_title");
+const about_message = document.getElementById("about_message");
+const main_service_tittle = document.getElementById("main-service-title");
+const example_1 = document.getElementById("example_1");
+const first_link = document.getElementById("first_link");
+const first_button = document.getElementById("first_button");
+const example_2 = document.getElementById("example_2");
+const second_link = document.getElementById("second_link");
+const second_button = document.getElementById("second_button");
+const example_3 = document.getElementById("example_3");
+const third_link = document.getElementById("third_link");
+const third_button = document.getElementById("third_button");
+const references = document.getElementById("references");
+const references_1 =document.getElementById("references_1");
+const references_2 = document.getElementById("references_2");
+const references_3 = document.getElementById("references_3");
+
+
+// Title of The website 
+title_page.innerHTML = "Eddy O. Gonzalez";
+
+// Main title
+main_title.innerHTML = "Welcome To My Porfolio Online";
+
+// Main message
+main_message.innerHTML = " I am a visionary student that is pursuing a Sofware Engineering Degreen in Brigham Young University Idaho. I have experience in the following lenguages: Python, C++, HTML, CSS, JavaScript, Swift, Linux, Mysql" ;
+
+// Quote 
+quote.innerHTML = "&ldquo;Learn as if you will live forever, live like you will die tomorrow. &rdquo; — Mahatma Gandhi";
+
+// Contact
+contact.innerHTML = "Contact Me";
+
+//about_title
+about_title.innerHTML = "About Me ";
+
+//about Message
+about_message.innerHTML = "Passion and diligence are two characteristics that I belive most reflect me as an individual. I belive this is the key to being successful in any goal that one set's their mind. I love to generate new ideas and solutions to relevant problems. I enjoy embracing the lessons learned from failure, stand up and continue to grown";
+
+// Main Services 
+main_service_tittle.innerHTML ="Main Projects";
+
+// Example 1 
+example_1.innerHTML = "Torpedo Game: The Torpedo game was code in Python and is a 2 player game using submarines and an underwater theme. Players will race to win and earn points by shooting obstacles or lose points by colliding with them. Points will be calculated and displayed in real-time.";
+
+//first link 
+first_link.action = "https://github.com/eddyog/submarine-game";
+
+//first button 
+first_button.innerHTML = "Submarine Github";
+
+//Example 2
+example_2.innerHTML = "Moon Lander is a genre of video games loosely based on the 1969 landing of the Apollo Lunar Module on the Moon. This program was made in c++ focusing in software-oriented programing";
+
+//Second Link
+second_link.action = "https://github.com/eddyog/submarine-game";
+
+// Second Button
+second_button.innerHTML = "Moon Lander Github";
+
+//Example 3
+example_3.innerHTML = "Xylophone Game: It was created using the program Xcode in the lenguage Swift. The UI interface was created with view controler Scene and the sounds were connected with IBActions";
+
+//Third Link 
+third_link.action = "https://github.com/eddyog/submarine-game";
+
+//Third button
+third_button.innerHTML = "Xylophone Github";
+
+//Others
+references.innerHTML ="Other Websites";
+
+//Others First Example
+references_1.innerHTML =  "Temple Inn & Suites";
+
+//Others Second Example
+references_2.innerHTML = "Weather Site";
+
+//I-Balance-app
+references_3.innerHTML = "I-Balances Planner"
+
+
+
+
+
+
+
---
+++ home/js/images.js
@@ -0,0 +1,38 @@
+
+
+const picture = document.getElementById('welcome-image');
+const banner  = document.getElementById('banner');
+const project_first_image = document.getElementById('project_first_image');
+const project_second_image = document.getElementById('project_second_image');
+const project_third_image = document.getElementById('project_third_image');
+const references_1_image = document.getElementById('references_1_image');
+const references_2_image = document.getElementById('references_2_image');
+const references_3_image = document.getElementById('references_3_image');
+
+//Banner
+banner.src = '/home/images/banner.jpg';
+
+//Profile Picture
+picture.src = '/home/images/eddy.jpg';
+
+//First Example Picture 
+project_first_image.src = '/home/images/torpedo.jpg';
+
+//Second Example Picture
+project_second_image.src = '/home/images/moon-lander.png';
+
+//Third Example Picture
+project_third_image.src = '/home/images/xylophone.png';
+
+//References First Image
+references_1_image.src = '/home/images/temple-inn.png';
+
+//References Second Image
+references_2_image.src = "/home/images/weather-site.jpg";
+
+//References Third Image
+references_3_image.src = "/home/images/i-balances.png";
+
+
+
+
