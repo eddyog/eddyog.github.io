31d13f43c12e1bd64c954e18704222449a9aa22a
Eddy Gonzalez <gon19005@byui.edu>
2022-03-22 13:32:17 -0600

    update langauges

--- languages/read-json.js
+++ languages/read-json.js
@@ -1,30 +1,49 @@
 
-var requestURL = "https://github.com/eddyog/eddyog.github.io/blob/main/json-files/python.json"
+
+
+var requestURL = "../json-files/python.json"
 var request = new XMLHttpRequest();
+
 
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.load = function() {
+request.onload = function() {
     var languageData = request.response;
     readLanguage(languageData);
+
 }
 
 
+
+
 function readLanguage(jsonObj){
-    var languages = jsonObj['Languages']
+
+    var languages = jsonObj['Python']
     var i = 0;
 
-    for (i = 0; i < languages.length ; i ++) {
+    var name = languages.name;
+    document.getElementById("projectName").innerHTML = name;
+
+     for (i = 0; i < languages.length ; i ++) {
         
-        var image = "<src='" + languages[i].imageSrc + "' alt='" + languages[i].imageAlt + "'>";
-        document.getElementById("project-image").innerHTML = image;
+    //     var image = "<src='" + languages[i].imageSrc + "' alt='" + languages[i].imageAlt + "'>";
+    //     document.getElementById("project-image").innerHTML = image;
 
-        var name = languages[i].name;
-        document.getElementById("projectName").innerHTML = name;
+    // const cards = document.getElementById("card");
+    // let listCard = document.createElement("div");
+    // listCard.textContent = "";
+    // cards.appendChild(listCard);
 
-        var description = languages[i].description;
-        document.getElementById("description").innerHTML = description;
+    const nameList = document.getElementById("projectName");
+    let listNames = document.createElement('p');
+    listNames.textContent = languages[i].name;
+    nameList.appendChild(listNames);
+
+    const descriptionList = document.getElementById("description");
+    let listDescription = document.createElement('p');
+    listDescription.textContent = languages[i].description;
+    descriptionList.appendChild(listDescription);
 
     }
 }
