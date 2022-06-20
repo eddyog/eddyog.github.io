27ee19aeddde3d70835de3e56b876c0c5560f82a
Eddy Gonzalez <gon19005@byui.edu>
2022-06-20 12:05:10 -0600

    update

--- languages/read-json.js
+++ languages/read-json.js
@@ -40,22 +40,11 @@ function readLanguage(jsonObj){
     const title_page = document.getElementById("title_page");
     title_page.innerHTML = "Eddy O. Gonzalez";
 
-
-
     var languages = jsonObj['Language'];
 
-//  var arr = [
-//     {name: 'Susan', country: 'Usa', age: 27},
-//     {name: 'Eddy', country: 'Guatema;a', age: 31},
-// ];
 
 var template = document.querySelector('#product-card-template');
 for (var i = 0; i < languages.length; i++){
-
-    // const nameList = document.getElementById("projectName");
-    // let listNames = document.createElement('p');
-    // listNames.textContent = languages[i].name;
-    // nameList.appendChild(listNames);
 
     var user = languages[i];
     var clone = template.content.cloneNode(true);
@@ -83,36 +72,4 @@ for (var i = 0; i < languages.length; i++){
 }
    
 
-
-    // var mytemp = document.getElementById("card");
-    // var clon = mytemp.content.cloneNode(true);
-    // mytemp.appendChild(clon);
-
-    
-
-    // const debug = document.getElementById("debugging");
-    // debug.innerHTML = "a";
-
-
-    // var name = languages.name;
-    // document.getElementById("projectName").innerHTML = name;
-
-    //  for (i = 0; i < languages.length ; i ++) {
-        
-    //     var image = "<src='" + languages[i].imageSrc + "' alt='" + languages[i].imageAlt + "'>";
-    //     document.getElementById("project-image").innerHTML = image;
-
-    // const cards = document.getElementById("card");
-    // let listCard = document.createElement("div");
-    // listCard.textContent = "";
-    // cards.appendChild(listCard);
-
-
-
-    // const descriptionList = document.getElementById("description");
-    // let listDescription = document.createElement('p');
-    // listDescription.textContent = languages[i].description;
-    // descriptionList.appendChild(listDescription);
-
-    // }
 }
